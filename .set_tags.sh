3582aafd0258bf3982df5a2e68dd4d681c85ce2d
Mark Ellis <memellis@gmail.com>
2018-06-01 00:16:32 +0100

    Debugging setting tags so deploy release can work part 5

--- .set_tags.sh
+++ .set_tags.sh
@@ -18,6 +18,7 @@ if [ "$TRAVIS_BRANCH" = "$BRANCH" ]; then
            # Add tag and push to master.
 
            git tag -a v${TRAVIS_BUILD_NUMBER} -m "Travis build $TRAVIS_BUILD_NUMBER pushed a tag." 
+	   git remote set-url orgin git@github.com:memellis/SlotPuzzle2d.git
            git push origin v${TRAVIS_BUILD_NUMBER}
 
            git tag
